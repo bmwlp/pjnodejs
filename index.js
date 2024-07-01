764306c5a530b83e4b4883a1f1e9415f4cbeaa5d
bmwlp <walaipan.baimon@gmail.com>
2024-07-01 16:15:37 +0700

    e

--- index.js
+++ index.js
@@ -84,6 +84,6 @@ app.delete('/users', (req, res) => {
     )
 })
 
-app.listen(process.env.PORT || 4008, () => {
+app.listen(process.env.PORT || 3008, () => {
     console.log('CORS-enabled web server listening on port 4000')
 })
